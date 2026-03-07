8b59d8cf31f441d37c7c64d5373254a0566abe21
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-03-07 23:00:13 +0400

    DML day 1

--- DML/SQLQuery2 - SELECT.sql
+++ DML/SQLQuery2 - SELECT.sql
@@ -1,8 +1,13 @@
 --SQLQuery2 - SELECT.sql
 
-USE PV_522_DDL;
+USE PV_522_Import;
 
-ALTER TABLE Groups
-ALTER COLUMN start_time TIME(0) NOT NULL
+--ALTER TABLE Groups
+--ALTER COLUMN start_time TIME(0) NOT NULL
 
-SELECT* FROM Groups;
+SELECT 
+		group_name  AS N'Группа',
+		direction_name AS N'Направление обучения'
+FROM	Groups, Directions
+WHERE	direction=direction_id
+;
