cb63f2ae1acd5133a9da925005e81c6be9dd39db
Romanhardy99 <144020402+Romanhardy99@users.noreply.github.com>
2026-03-12 23:01:32 +0400

    Insert Schedule DONE

--- DML/SQLQuery4 - COUNT.sql
+++ DML/SQLQuery4 - COUNT.sql
@@ -2,11 +2,30 @@
 
 USE PV_522_Import;
 
+--SELECT
+--			group_name		AS	N'Имя группы',
+--			COUNT(stud_id)	AS	N'Количество студентов'
+--FROM		Students, Groups
+--WHERE		[group]=group_id
+--GROUP BY	group_name
+--HAVING		COUNT(stud_id) < 10
+--;
+
+--SELECT
+--		[Направление обучения]	=	direction_name,
+--		[Количество групп]		=	COUNT(DISTINCT group_id),
+--		[Количество студентов]	=	COUNT(stud_id)
+--FROM Students, Groups, Directions
+--WHERE [group]	=	group_id
+--AND	direction = direction_id
+--GROUP BY	direction_name
+--;
+
 SELECT
-			group_name		AS	N'Имя группы',
-			COUNT(stud_id)	AS	N'Количество студентов'
-FROM		Students, Groups
-WHERE		[group]=group_id
-GROUP BY	group_name
-HAVING		COUNT(stud_id) < 10
+
+	 [Направление обучение]		=	direction_name,
+	 [Количество групп]			=	(SELECT COUNT(group_id)	FROM Groups WHERE direction=direction_id),
+	 [Количество студентов]		=	(SELECT COUNT(stud_id)	FROM Students, Groups WHERE [group]=group_id AND direction=direction_id)
+FROM  Directions
+--WHERE	direction=direction_id
 ;
